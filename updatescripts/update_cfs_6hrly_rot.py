fa50a33185ef391d18e5b9bc47a2aec8a0f24be8
Rémi Cousin <remic@iri.columbia.edu>
2026-03-13 09:55:38 -0400

    new version of update script

--- updatescripts/update_cfs_6hrly_rot.py
+++ updatescripts/update_cfs_6hrly_rot.py
@@ -3,42 +3,62 @@
 import update_utilities as uu
 from pathlib import Path
 import datetime
-import urllib.request as urlr
-import re
+import urllib
 import shutil
+from dateutil.relativedelta import relativedelta
+import pandas as pd
 
 
 URL_PATH = "https://nomads.ncep.noaa.gov/pub/data/nccf/com/cfs/prod"
 DEST_DIR = Path("/Data/data22/noaa/ncep/cfsv2/6_hourly_rotating")
-TODAY = datetime.datetime.today()
+TODAY = datetime.date.today() + relativedelta(hour=18)
 
 # Rotating archive holds last 7 days
-for adate in [(TODAY - datetime.timedelta(days=d)) for d in range(7)]:
-    adate_Ymd = adate.strftime("%Y%m%d")
-    for anhour in range(0, 24, 6):
-        for amember in range(1, 5):
-            rel_path = f"cfs.{adate_Ymd}/{anhour:02}/6hrly_grib_{amember:02}"
-            dest_dir = DEST_DIR / rel_path
-            url_path = f"{URL_PATH}/{rel_path}"
-            try:
-                with urlr.urlopen(url_path) as r:
-                    if r.status != 200:
-                        message = f'Trying to get {rel_path} returned status {r.status}'
-                        file_names = []
+for var in ["pgbf", "flxf"]:
+    for astart in pd.date_range(end=TODAY, periods=7*4, freq="6h")[::-1]:
+        hourly_path = f"cfs.{astart.strftime("%Y%m%d")}/{astart.strftime("%H")}"
+        done_file = DEST_DIR / hourly_path / f"{var}_done"
+        if done_file.is_file():
+            print(f"{hourly_path} is already complete for {var}")
+        else:
+            is_done = True
+            for amember in range(1, 5):
+                mem_path = f"{hourly_path}/6hrly_grib_{amember:02}"
+                dest_dir = DEST_DIR / mem_path
+                url_path = f"{URL_PATH}/{mem_path}"
+                try:
+                    url_resp = urllib.request.urlopen(url_path)
+                    is_url_path = True
+                except urllib.error.HTTPError as e:
+                    print(f"could not open {url_path} because of {e}")
+                    is_url_path = False
+                    is_done = False
+                if is_url_path:
+                    if amember == 1 :
+                        # 1st members lead up to 
+                        # 0th hour of first day 7 months later
+                        last_L = astart + relativedelta(months=7, day=1, hour=0)
+                    elif astart.hour == 0 :
+                        # Midnight starts other than for 1st member lead up to 
+                        # 0th hour of first day 4 months later
+                        last_L = astart + relativedelta(months=4, day=1, hour=0)
                     else:
-                        url_text = r.read().decode()
-                        file_names = re.findall(
-                            r'<a href="((?:pgbf|flxf)\d{10}.\d{2}.\d{10}.grb2)">',
-                            url_text,
+                        # All other (ie not 1st member not midnight starts) lead up to 
+                        # 45 days
+                        last_L = astart + relativedelta(days=45)
+                    for L_date in pd.date_range(start=astart, end=last_L, freq="6h"):
+                        file_name = (
+                            f"{var}{L_date.strftime("%Y%m%d%H")}."
+                            f"{amember:02}.{astart.strftime("%Y%m%d%H")}.grb2"
                         )
-            except Exception as e:
-                file_names = []
-                message = f'Trying to get {rel_path} returned status {e}'
-            for file_name in file_names:
-                is_downloaded, message = uu.download_file(
-                    dest_dir, file_name, f'{url_path}/{file_name}',
-                )
-                print(message)
+                        print(file_name)
+                        download_status = uu.download_file(
+                            dest_dir, file_name, f'{url_path}/{file_name}',
+                        )
+                        print(download_status["message"])
+                        is_done = is_done & (download_status["flag"] >= 0)
+            if is_done:
+                done_file.touch(mode=uu.FILE_MODE)
 
 # Ingrid tries to read last 2 weeks from today only
 old_date = TODAY - datetime.timedelta(days=15)
