3a0cda8a0ee9c33d9719ebfb00691bf0627090e1
Jeff Turmelle <jefft@iri.columbia.edu>
2026-03-24 11:26:14 -0400

    only do actual_size_offset if actuial_size exists.

--- updatescripts/S2S/check_file_size.py
+++ updatescripts/S2S/check_file_size.py
@@ -4,12 +4,12 @@ import logging
 def process_file_by_size(filename, min_size, actual_size, dryrun=False):
     # Check file size and remove if it doesn't match the expected size
     size = 0
-    # Give or take 5% of the actual size
-    actual_size_offset = int(actual_size*.05)
     if os.path.exists(filename):
         size = os.path.getsize(filename)
         # if actual_size == 0, we don't know the exact size, so don't check it.
         if actual_size is not None and 0 < actual_size != size:
+            # Give or take 5% of the actual size
+            actual_size_offset = int(actual_size * .05)
             if actual_size-actual_size_offset < size < actual_size+actual_size_offset:
                 return size
             else:
